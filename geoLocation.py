5f92d57fe157132fe876e60d57d214fb2fc0e7d2
jviso <jviso@jvisos-MBP.dhcp.lexmark.com>
2016-04-07 16:49:59 -0400

    cleaning geolocations a bit more.. Soon will link it to main

--- geoLocation.py
+++ geoLocation.py
@@ -1,11 +1,23 @@
 from geopy.geocoders import Nominatim
 from geopy import *
+import urllib2
+import urllib
+import requests
 
-geolocator = Nominatim()
-whereToPrompt = raw_input("What address do you want to get the latitude and longitude of? ")
-location = geolocator.geocode(whereToPrompt)
-if location:
-	print location.address
-	print((location.latitude, location.longitude))
-else:
-	print "Not able to get address, try again"
+def getIPAddress():
+	return requests.get('https://api.ipify.org/?format=json').json()["ip"]
+
+def getCurrentAddress():
+	ipAddr = getIPAddress()
+	return urllib.urlopen('http://api.hostip.info/get_html.php?ip=' + ipAddr + '&position=true').read()
+
+def getLatLongitudes():
+	geolocator = Nominatim()
+	whereToPrompt = raw_input("What address do you want to get the latitude and longitude of? ")
+	location = geolocator.geocode(whereToPrompt)
+	if location:
+		#print currentAddress + "\n"
+		print location.address + "\n"
+		print((location.latitude, location.longitude))
+	else:
+		print "Not able to get address, try again"
