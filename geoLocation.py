167036d48d6bd88c4d6b34e8b27da58741e3d2fc
jviso <jviso@jvisos-MBP.dhcp.lexmark.com>
2016-04-07 14:45:47 -0400

    small details forgot to push earlier

--- geoLocation.py
+++ geoLocation.py
@@ -2,7 +2,8 @@ from geopy.geocoders import Nominatim
 from geopy import *
 
 geolocator = Nominatim()
-location = geolocator.geocode("Lexmark, Lexington, KY")
+whereToPrompt = raw_input("What address do you want to get the latitude and longitude of? ")
+location = geolocator.geocode(whereToPrompt)
 if location:
 	print location.address
 	print((location.latitude, location.longitude))
