65986c154f6008d3b042dd2d75ff6e6ff22d705b
jviso <jviso@jvisos-MBP.dhcp.lexmark.com>
2016-04-08 15:07:30 -0400

    cleaned things up more with geoLocations

--- geoLocation.py
+++ geoLocation.py
@@ -1,27 +1,33 @@
 from geopy.geocoders import Nominatim
 from geopy import *
-import urllib2
 import urllib
 import requests
 
-def getIPAddress():
-	return requests.get('https://api.ipify.org/?format=json').json()["ip"]
 
-def getCurrentAddress():
-	ipAddr = getIPAddress()
-	return urllib.urlopen('http://api.hostip.info/get_html.php?ip=' + ipAddr + '&position=true').read()
+class geoLocations(object):
+    def getIPAddress(self):
+        return requests.get('https://api.ipify.org/?format=json').json()["ip"]
 
-def getLatLongitudes():
-	geolocator = Nominatim()
-	currentLocation = raw_input("What's your current address? ")
-	whereToPrompt = raw_input("\nWhat address do you want to get the latitude and longitude of? ")
-	myLocation = geolocator.geocode(currentLocation)
-	destination = geolocator.geocode(whereToPrompt)
-	if location:
-		#print currentAddress + "\n"
-		print myLocation.address + "\n"
-		print((myLocation.latitude, myLocation.longitude))
-		print destination.address + "\n"
-		print ((destination.latitude, destination.longitude))
-	else:
-		return "Not able to get address, try again"
+    def getCurrentAddress(self):
+        ipAddr = geoLocations.getIPAddress()
+        return urllib.urlopen('http://api.hostip.info/get_html.php?ip=' + ipAddr + '&position=true').read()
+
+    def getMyAddress(self):
+        geolocator = Nominatim()
+        currentLocation = raw_input("What's your current address? ")
+        myLocation = geolocator.geocode(currentLocation)
+
+        if myLocation:
+            return myLocation.latitude, myLocation.longitude
+        else:
+            return "Not able to get address, try again."
+
+    def getMyDestination(self):
+        geolocator = Nominatim()
+        whereToPrompt = raw_input("What address do you want to get the latitude and longitude of? ")
+        destination = geolocator.geocode(whereToPrompt)
+
+        if destination:
+            return destination.latitude, destination.longitude
+        else:
+            return "Not able to get address, try again."
