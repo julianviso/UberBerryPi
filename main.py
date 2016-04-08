75b625e4e82756fdc0140c2bf82281c75e1ece11
jeshall <jessehall1989@gmail.com>
2016-04-08 11:43:28 -0400

    remove old calls to price estimate

--- main.py
+++ main.py
@@ -84,33 +84,6 @@ def get_products():
 
     return str(response.json)
 
-#Bugged Call to SDK /v1/estimates/price
-
-@app.route("/Xget_priceX")
-def get_price_estimates():
-    if not isClientValid():
-        return "Please create an uber session and client at http://localhost:5000\n"
-    print "Requesting price estimates from Uber"
-    try:
-        response = CLIENT.get_price_estimates(MY_LATITUDE, MY_LONGITUDE, DEST_LATITUDE, DEST_LONGITUDE)
-    except Exception as e:
-        print traceback.format_exc()
-    print "Price estimates recieved"
-    return str(response.json)
-
-@app.route("/custom_price")
-def get_custom_price_estimates():
-    headers = {'Authorization': 'Bearer {0}'.format(SESSION.oauth2credential.access_token)}
-    params = {"start_latitude": MY_LATITUDE,
-    "start_longitude": MY_LONGITUDE,
-    "end_latitude": DEST_LATITUDE,
-    "end_longitude": DEST_LATITUDE}
-    try:
-        r = requests.get("https://{0}/v1/estimates/price".format(CLIENT.api_host), params=params,  headers=headers)
-    except Exception as e:
-        print traceback.format_exc()
-    return r.text
-
 @app.route("/get_price")
 def get_price():
     price_estimates_request = UberRequest.price_estimates(CLIENT.api_host, SESSION, MY_LATITUDE, MY_LONGITUDE, DEST_LATITUDE, DEST_LONGITUDE)
